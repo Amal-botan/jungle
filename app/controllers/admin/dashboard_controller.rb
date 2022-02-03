1b378fd7cd7ea42cecfd7694c69b364657f86478
Amal Botan <amalbotan254@gmail.com>
2022-02-03 20:03:45 +0000

    Fixed admin authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["ADMIN_USER_KEY"], password: ENV["ADMIN_SECRET_KEY"]
+
   def show
+    @category_count = Category.count
+    @product_count = Product.count
   end
 end
