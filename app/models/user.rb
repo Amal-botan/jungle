f235d81cbd9783dbfe35db512f44ce974e1c52de
Amal Botan <amalbotan254@gmail.com>
2022-02-08 06:58:04 +0000

    Added testing authentication for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
   has_secure_password
   
+  validates :name, presence: true
   validates :password, presence: true, confirmation: true, length: {minimum: 6}
   validates :password_confirmation, presence: true
   validates :email, uniqueness: {case_sensitive: false}
   
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip)
+    user && user.authenticate(password)
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,20 +43,28 @@ RSpec.describe User, type: :model do
 
     end
 
-    
 
-    # it "user password must be above 6 characters" do
-    #   @user = User.new(name: "Amal", email: "A@gmail.com", password: "bye", password_confirmation: "bye")
-    #   expect(@user).to_not be_valid
-    #   expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
-
-    # end
+    it "user password must be above 6 characters" do
+      @user = User.new(name: "Amal", email: "A@gmail.com", password: "bye", password_confirmation: "bye")
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
 
   end
 
-  describe '.authenticate_with_credentials' do
+  describe 'authenticate_with_credentials' do
     # examples for this class method here
-    
+    it "user must be authenticated with credentials" do
+      @user = User.create(name: "Amal", email: "A@gmail.com", password: "password", password_confirmation: "password")
+      @user1 = User.authenticate_with_credentials("A@gmail.com", "password")
+      expect(@user).to eq(@user1)
+    end
+
+    it "user must be authenticated with credentials even if they add white spaces" do
+      @user = User.create(name: "Amal", email: "A@gmail.com", password: "password", password_confirmation: "password")
+      @user1 = User.authenticate_with_credentials(" A@gmail.com ", "password")
+      expect(@user).to eq(@user1)
+    end
   end
 
 end
