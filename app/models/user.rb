b5022520f598ae16d2a79b897be804eddd9ae879
Amal Botan <amalbotan254@gmail.com>
2022-02-08 06:12:12 +0000

    Added testing validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   
+  validates :password, presence: true, confirmation: true, length: {minimum: 6}
+  validates :password_confirmation, presence: true
+  validates :email, uniqueness: {case_sensitive: false}
+  
 end
